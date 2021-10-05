913966b3abca092ccf68eb8f79597de51c5e8f76
Sejin Kim <sejjj120@gmail.com>
2021-10-05 16:59:24 +0900

    가상머신 테스트 분리

--- vm/install-youki.sh
+++ vm/install-youki.sh
@@ -8,10 +8,9 @@ sudo apt-get install -y \
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source ~/.cargo/env
 
-cd ~
-git clone https://github.com/containers/youki
-cd youki
+git clone https://github.com/containers/youki ~/youki
+cd ~/youki
 bash build.sh
 sudo mv target/x86_64-unknown-linux-gnu/debug/youki /usr/local/bin/
-cd ..
+cd ~
 rm -rf youki
